4bf10b2f12f88e27a1f25cfcf1af255e7871fcc8
Rushal <TheRushal@gmail.com>
2018-08-05 17:39:17 -0500

    start lecture 72

--- Rushal RPG/Assets/Enemies/Enemy.cs
+++ Rushal RPG/Assets/Enemies/Enemy.cs
@@ -3,26 +3,60 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.ThirdPerson;
 
-public class Enemy : MonoBehaviour
+public class Enemy : MonoBehaviour, IDamagable
 {
 	[SerializeField] float maxHealthPoints = 100f;
-	[SerializeField] float attackRadius= 5f;
-
-	float currentHealthPoints = 100f;
+	[SerializeField] float attackRadius = 5f;
+	[SerializeField] float damagePerShot = 9f;
+	[SerializeField] float secondsBetweenShots = 0.5f;
+	[SerializeField] float chaseRadius= 5f;
+	[SerializeField] GameObject projectileToUse;
+	[SerializeField] GameObject projectileSocket;
+	[SerializeField] Vector3 aimOffset = new Vector3( 0f, 1f, 0f);
+	
+	bool isAttacking = false;
+	float currentHealthPoints;
 	AICharacterControl aiCharacterControl = null;
 	Player player;
+
+	public float healthAsPercentage
+	{
+		get
+		{
+			return currentHealthPoints / maxHealthPoints;
+		}
+	}
+
+	public void TakeDamage( float damage )
+	{
+		currentHealthPoints = Mathf.Clamp( currentHealthPoints - damage, 0f, maxHealthPoints );
+		if ( currentHealthPoints <= 0 ) { Destroy( gameObject ); }
+	}
 
 	void Start () {
 		aiCharacterControl = GetComponent<AICharacterControl> ();
 		player = FindObjectOfType<Player> ();
 
+		currentHealthPoints = maxHealthPoints;
 	}
 	
 	void Update () {
 
 		float distanceToPlayer = Vector3.Distance ( player.transform.position, aiCharacterControl.transform.position );
 
-		if ( distanceToPlayer <= attackRadius )
+		if ( distanceToPlayer > attackRadius )
+		{
+			isAttacking = false;
+			CancelInvoke();
+		}
+
+		if ( distanceToPlayer <= attackRadius && !isAttacking )
+		{
+			isAttacking = true;
+			InvokeRepeating("SpawnProjectile", 0, secondsBetweenShots); //TODO switch to coroutine
+		}
+
+		if ( distanceToPlayer <= chaseRadius )
 		{
 			aiCharacterControl.SetTarget ( player.transform );
 		}
@@ -33,16 +67,23 @@ public class Enemy : MonoBehaviour
 
 	}
 
-	public float healthAsPercentage
+	void SpawnProjectile ()
 	{
-		get
-		{
-			return currentHealthPoints / maxHealthPoints;
-		}
+		GameObject projectile = Instantiate( projectileToUse, projectileSocket.transform.position, Quaternion.identity );
+		Projectile projectileComponent = projectile.GetComponent<Projectile>();
+		projectileComponent.SetDamage( damagePerShot );
+
+		Vector3 unitVectorToPlayer = ( player.transform.position + aimOffset - projectileSocket.transform.position ).normalized;
+		float projectileSpeed = projectileComponent.projectileSpeed;
+		projectile.GetComponent<Rigidbody>().velocity = unitVectorToPlayer * projectileSpeed;
 	}
 
 	private void OnDrawGizmos ()
 	{
+		// draw move
+		Gizmos.color = new Color( 0f, 0f, 255f, .5f );
+		Gizmos.DrawWireSphere( transform.position, chaseRadius );
+
 		// draw attack
 		Gizmos.color = new Color ( 255f, 0f, 0f, .5f );
 		Gizmos.DrawWireSphere ( transform.position, attackRadius );
--- Rushal RPG/Assets/Player/Player.cs
+++ Rushal RPG/Assets/Player/Player.cs
@@ -2,14 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public class Player : MonoBehaviour, IDamagable
 {
+	[SerializeField] int enemyLayer = 10;
 	[SerializeField] float maxHealthPoints = 100f;
+	[SerializeField] float meleeDamagePerClick = 10f;
+	[SerializeField] float minTimeBetweeHits = 0.5f;
+	[SerializeField] float maxAttackRange = 2f;
 
-	float currentHealthPoints = 100f;
+	CameraRaycaster cameraRaycaster;
+	GameObject currentTarget;
+	float currentHealthPoints;
+	float lastHitTime = 0f;
 
 	void Start () {
-		
+		cameraRaycaster = FindObjectOfType<CameraRaycaster>();
+		cameraRaycaster.notifyMouseClickObservers += OnMouseClick;
+
+		currentHealthPoints = maxHealthPoints;
 	}
 	
 	void Update () {
@@ -22,5 +32,34 @@ public class Player : MonoBehaviour
 		{
 			return currentHealthPoints / maxHealthPoints;
 		}
+	}
+
+	void OnMouseClick ( RaycastHit raycastHit, int layerHit )
+	{
+		if ( layerHit == enemyLayer )
+		{
+			var enemy = raycastHit.collider.gameObject;
+			
+			if ( (enemy.transform.position - transform.position).magnitude > maxAttackRange ) { return; }
+
+			currentTarget = enemy;
+
+			Vector3 faceTheEnemy = ( enemy.transform.position - transform.position );
+			transform.rotation = Quaternion.Slerp( transform.rotation, Quaternion.LookRotation( faceTheEnemy ), 0.2f );
+
+			IDamagable damagableComponent = enemy.GetComponent<IDamagable>();
+			if ( damagableComponent != null && Time.time - lastHitTime > minTimeBetweeHits )
+			{
+				damagableComponent.TakeDamage( meleeDamagePerClick );
+				lastHitTime = Time.time;
+			}
+
+		}
+	}
+
+	public void TakeDamage( float damage )
+	{
+		currentHealthPoints = Mathf.Clamp( currentHealthPoints - damage, 0f, maxHealthPoints );
+		//if ( currentHealthPoints <= 0 ) { Destroy( gameObject );  }
 	}
 }
